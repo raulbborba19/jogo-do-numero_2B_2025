d34bef6422b3c3887b70309d7a7f37cd81ebd0b3
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-08-07 07:42:39 -0300

    .

--- main.js
+++ main.js
@@ -2,4 +2,16 @@
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
 const baixoOuAlto = document.querySelector('.baixoOuAlto');
-const envioPalpite
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+
+
+
+function verificarPalpite() {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpite === 1) {
+        palpites.textContent = "Palpites anteriores: "
+    }
+
+palpites.textContent += palpiteUsuario + " ";
+
