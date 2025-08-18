9a6b566ac1bb7ef2e3e385722327aa96137f8d2a
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-08-18 10:22:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,28 @@ if (palpiteUsuario === numeroAleatorio) {
     ultimoResultado.textContent = "Parabéns! Você Acertou!";
     
 }
+
+contagemPalpites++;
+campoPalpite.value = "";
+campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReiniciar = document.createElement('bottomm')
+    document.body.appendChild(botaoReiniciar);
+    botaoReiniciar.textContent = 'Reiniicar Jogo';
+    botaoReiniciar.classList.add('botaoReiniciar');
+    document.body.appendChild(botapReiniciar);
+    botaoReiniciar.addEventListener('click', reinciiarJogo);
+}
+
+function reiniciarjogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciarr = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafoReiniciar of paragrafosReinciar)
+        paragrafosReiniciar.textcontent = "";
+}
