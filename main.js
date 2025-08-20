f957798917e5de46c4413e5ca303d13d51268188
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-08-20 07:33:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
+let numeroAleatorio = Math.flor(Math.random() *100) +1;
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
 const baixoOuAlto = document.querySelector('.baixoOuAlto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
-
-
+let contagemPalpites = 1;
+let botaoReiniciar;
 
 function verificarPalpite() {
     const palpiteUsuario = Number(campoPalpite.value);
@@ -17,7 +17,21 @@ palpites.textContent += palpiteUsuario + " ";
 
 if (palpiteUsuario === numeroAleatorio) {
     ultimoResultado.textContent = "Parabéns! Você Acertou!";
-    
+    ultimoResultado.style.backgroundColor = "green";
+    baixoOuAlto.textContent = "";
+    finalizarJogo();
+} else if (contagemPalpites === 10) {
+    ultimoResultado.textContent = "FIM DE JOGO! !";
+    baixoOuAlto.textContent = "":
+    finalizarJogo()
+} else {
+    ultimoResultado.textContent = "Errado";
+    ultimoResultado.style.backgroundColor = "red";
+    if (palpiteUsuario < numeroAleatorio) {
+        baixoOuAlto.textContent = "o último palpite foi muito baixo":
+    } else if (palpiteUsuario > numeroAleatorio) {
+        baixoOuAlto.textContent = "o último palpite foi muito alto":
+    }
 }
 
 contagemPalpites++;
@@ -34,15 +48,14 @@ function finalizarJogo() {
     document.body.appendChild(botaoReiniciar);
     botaoReiniciar.textContent = 'Reiniicar Jogo';
     botaoReiniciar.classList.add('botaoReiniciar');
-    document.body.appendChild(botapReiniciar);
     botaoReiniciar.addEventListener('click', reinciiarJogo);
 }
 
 function reiniciarjogo() {
     contagemPalpites = 1;
-    const paragrafosReiniciarr = document.querySelectorAll('.paragrafosResultado p');
+    const paragrafosReiniciarr = document.querySelectorAll('.paragrafosResultados p');
     for (const paragrafoReiniciar of paragrafosReinciar)
-        paragrafosReiniciar.textcontent = "";
+        paragrafosReiniciar.textContent = "";
 }
 
 botaoReiniciar.parentNode.removeChild(botaoReiniciar);
@@ -51,4 +64,5 @@ envioPalpite.disabled = false;
 campoPalpite.value = "";
 campoPalpite.focus();
 ultimeResultado.style.backgroundColor = 'white';
-numeroAleatorio = Math.floor(Math.random())
+numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+}
